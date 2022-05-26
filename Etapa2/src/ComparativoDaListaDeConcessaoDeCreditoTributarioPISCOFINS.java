1a58c35721d53ee79f64501453d3c107a9442ed9
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-05-26 19:46:36 -0300

    4BlankBasicFiles
    Created Main file with a short menu and 3 dummy classes that will be improved in the future

---
+++ Etapa2/src/ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS.java
@@ -0,0 +1,7 @@
+package Etapa2.src;
+
+public class ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS {
+    public static void main(String[] args) {
+        System.out.println("ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS");
+    }
+}
