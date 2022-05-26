e3a569f3f5464fb60fe41fe8dccce13e9654fb9c
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-05-26 19:27:45 -0300

    Created ReadCSV file
    To read and display the entire CSV inside java console

---
+++ Etapa2/Testes/ReadCSV.java
@@ -0,0 +1,28 @@
+package Etapa2.Testes;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadCSV {
+    public static void main(String[] args) {
+        final String FILE_PATH = "C:/Users/felip/Desktop/log/PUCRS/IT16/Etapa2/TA_PRECO_MEDICAMENTO.csv";
+        File CSVFile = new File(FILE_PATH);
+
+        try {
+
+            String fileLines = new String();
+
+            Scanner reader = new Scanner(CSVFile);
+
+            while (reader.hasNext()) {
+                fileLines = reader.nextLine();
+
+                System.out.println(fileLines);
+            }
+
+        } catch (FileNotFoundException e) {
+            
+        }
+
+    }
+}
