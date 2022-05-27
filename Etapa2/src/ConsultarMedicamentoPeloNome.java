e9c3464cc771fc4e95ff8d658ffa7be7d87597cb
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-05-27 17:44:52 -0300

    NewMethods
    On "ConsultarMedicamentoPeloNome" file, created two methods; one for asking for the name of the medicine and the other for looking for it on the csv file.
    
    On "Main" created a Method for the Menu Interface and another for the chioce as well as another for handling said choice.
    
    On "ReadCSV.java", under Testes, improved content visualization and numbering of rows and columns

--- Etapa2/src/ConsultarMedicamentoPeloNome.java
+++ Etapa2/src/ConsultarMedicamentoPeloNome.java
@@ -1,53 +1,65 @@
 package Etapa2.src;
+
+// Imports
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/*
+    IMPORTANTE
+    LEMBRAR DE USAR .CONTAINS()
+*/
 public class ConsultarMedicamentoPeloNome {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            String nomeMedicamento;
-
-            System.out.print("Digite o nome do medicamento: ");
-            nomeMedicamento = input.nextLine();
-
-            consultarMedicamento(nomeMedicamento);
-        }
+        Scanner input = new Scanner(System.in);
+        consultarMedicamento(nomeMedicamento(input));
     }
 
-	public static void consultarMedicamento(String name) {
-		
-		final String FILE_PATH = "C:/Users/felip/Desktop/log/PUCRS/IT16/Etapa2/TA_PRECO_MEDICAMENTO.csv";
-		File arquivoCSV = new File(FILE_PATH);
-    
+    private static String nomeMedicamento(Scanner input) {
+        String nomeMedicamento;
+
+        System.out.print("Digite o nome do medicamento: ");
+        nomeMedicamento = input.nextLine();
+
+        return nomeMedicamento;
+    }
+
+    public static void consultarMedicamento(String name) {
+
+        final String FILE_PATH = "D:/Programming/GitHub/IT16/Etapa2/TA_PRECO_MEDICAMENTO.csv";
+        File arquivoCSV = new File(FILE_PATH);
+
         try {
-            String fileLine = new String();
+            String fileLine = "";
             boolean haMedicamento = false;
+
             try (Scanner reader = new Scanner(arquivoCSV)) {
+                // Skip header
                 reader.nextLine();
-                
+
+                // While there are lines to be read
                 while (reader.hasNext()) {
                     fileLine = reader.nextLine();
-                    
+
                     String[] valoresEntreVirgulas = fileLine.split(";");
-                    
+
                     System.out.println("/===================================================\\");
-                    System.out.println("| Dados do medicamento "+name);
+                    System.out.println("| Dados do medicamento " + name);
                     System.out.println("| Nome: " + valoresEntreVirgulas[0]);
                     System.out.println("| Produto: " + valoresEntreVirgulas[9]);
                     System.out.println("| Apresentação: " + valoresEntreVirgulas[10]);
                     System.out.println("| Valor PF Sem Impostos: " + valoresEntreVirgulas[14]);
                     System.out.println("\\===================================================/");
                     haMedicamento = true;
-                    break;
                 }
-                
+
                 if (!haMedicamento) {
                     System.out.println("Infelizmente, não há medicamentos.");
                 }
-                
+
             }
         } catch (FileNotFoundException e) {
+            System.out.println(e);
         }
-	}
+    }
 }
