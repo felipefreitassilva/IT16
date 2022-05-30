58b72edc7572e46441bfe60fe172a5a0ec60b8d1
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-05-29 22:31:01 -0300

    Improved search by bar code Class
    Included import for using ArrayList
    Altered file path from static to relative
    Altered file separation character from , to ;
    Improved searching method so as to remove errors
    Added lookAgain method for prompting the user for looking again in case the desired medicament wasn't found as well as a method for calculating highest and lowest values

--- Etapa2/Main/src/BuscaPeloCodigoDeBarras.java
+++ Etapa2/Main/src/BuscaPeloCodigoDeBarras.java
@@ -2,12 +2,7 @@ package Etapa2.Main.src;
 
 import java.io.File;
 import java.util.Scanner;
-
-/*
-    1.	[Buscar pelo código de barras] O programa deverá solicitar ao usuário o número correspondente ao código de barras de um produto (coluna de dados “EAN 1”, por exemplo ‘525516020019503’) e então:
-    a.	Localizar todos os registros referentes a este produto, independentemente de terem sido comercializados ou não em 2020;
-    b.	Dentre todos os registos encontrados, identificar o Preço Máximo ao Consumidor (alíquota de 0%, coluna de dados “PMC 0%”) mais alto e o mais baixo. Exibir na tela o mais alto, o mais baixo e a diferença entre eles.
-*/
+import java.util.ArrayList;
 
 public class BuscaPeloCodigoDeBarras {
 
@@ -26,13 +21,14 @@ public class BuscaPeloCodigoDeBarras {
     public void lookForMedicine() {
         String barCode = getBarCode();
 
-        final String FILE_PATH = "D:/Programming/GitHub/IT16/Etapa2/TA_PRECO_MEDICAMENTO.csv";
+        final String FILE_PATH = "Etapa2\\TA_PRECO_MEDICAMENTO.csv";
         File csvfile = new File(FILE_PATH);
 
         String fileLine;
-        String regex = ",";
+        String regex = ";";
         String[] fileLineContent;
         boolean foundMedicine = false;
+        ArrayList<Double> pmcAmount = new ArrayList<Double>();
 
         try (Scanner reader = new Scanner(csvfile)) {
             // skip header when scanning
@@ -42,26 +38,88 @@ public class BuscaPeloCodigoDeBarras {
                 fileLine = reader.nextLine();
                 fileLineContent = fileLine.split(regex);
 
-                if (fileLineContent[3].equals(barCode)) {
-                    medicineInfo(fileLineContent, barCode);
-                    foundMedicine = true;
+                // since pmc values are on column 23, I need the product to have at least this
+                // many columns
+                if (fileLineContent.length > 23) {
+
+                    // all numbers on the csv file were converted to String by using ' character
+                    // before, and they are now being removed
+                    for (int i = 3; i <= 7; i++) {
+                        fileLineContent[i] = fileLineContent[i].substring(1);
+                    }
+
+                    if (fileLineContent[5].equals(barCode)) {
+                        // converting the String containing the pmc 0% value to a number
+                        double pmc = Double.parseDouble(fileLineContent[23].replace(",", "."));
+                        pmcAmount.add(pmc);
+                        foundMedicine = true;
+                    }
                 }
             }
 
-            if (!foundMedicine) {
+            if (foundMedicine) {
+                medicineInfo(extremePmc(pmcAmount), barCode);
+            } else {
                 System.out.printf("Infelizmente, não foram encontrados medicamentos por \"%s\".%n", barCode);
+                lookAgain();
             }
+
         } catch (Exception e) {
-            System.out.println("Houve um erro na leitura do arquivo. Tente novamente.");
+            System.out.println("Houve um erro na leitura do arquivo. Tente novamente." + e);
         }
     }
 
-    // method that displays more information on the medicine
-    private void medicineInfo(String[] fileLineContent, String barCode) {
-        System.out.printf("%n Dados do medicamento encontrado por \"%s\" %n", barCode);
+    // method to sort the highest and lowest pmc values
+    private double[] extremePmc(ArrayList<Double> pmcAmount) {
+        double[] extremePmc = new double[2];
+        extremePmc[0] = Double.MAX_VALUE;
 
-        for (String info : fileLineContent) {
-            System.out.println("| " + info);
+        for (double pmc : pmcAmount) {
+            if (pmc >= extremePmc[1]) {
+                extremePmc[1] = pmc;
+            }
+            if (pmc <= extremePmc[0]) {
+                extremePmc[0] = pmc;
+            }
+        }
+
+        return extremePmc;
+    }
+
+    // method that displays more information on the medicine
+    private void medicineInfo(double[] extremePmc, String barCode) {
+        double highestPmc = extremePmc[1];
+        double lowestPmc = extremePmc[0];
+        double pmcDifference = highestPmc - lowestPmc;
+        System.out.printf("%n Dados dos medicamentos encontrado por \"%s\" %n", barCode);
+
+        System.out.printf("| PMC mais alto: %.2f %n", highestPmc);
+        System.out.printf("| PMC mais baixo: %.2f %n", lowestPmc);
+        System.out.printf("| Diferença entre PMC's: %.2f %n", pmcDifference);
+    }
+
+    // method to ask the user if he wished to try another search
+    private void lookAgain() {
+        Scanner input = new Scanner(System.in);
+        String userResponse;
+        System.out.println("Buscar novamente? (Sim / Nao)");
+        userResponse = input.nextLine();
+        if (handleUserResponse(userResponse)) {
+            lookForMedicine();
+        }
+    }
+
+    // method to analyse user response and determin if program should run again
+    private boolean handleUserResponse(String userResponse) {
+        userResponse = userResponse.toUpperCase();
+        switch (userResponse) {
+            case "SIM":
+                return true;
+            case "NAO":
+                return false;
+            default:
+                System.out.println("Valor inválido. Tente novamente.");
+                return false;
         }
     }
 }
