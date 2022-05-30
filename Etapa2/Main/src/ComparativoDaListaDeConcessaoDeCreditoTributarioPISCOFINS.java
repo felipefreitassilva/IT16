dd43b751dd519d58a585ecad8427d1b3938a8655
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-05-29 22:35:48 -0300

    Improved comparing and graphical method
    Altered file path from static to relative
    Altered splitting character from , to ;
    Added "protective" if" statement for impeding bugs on lines with insufficient information
    Created a method for calculating percentages instead of making it all inside the same method that displays them

--- Etapa2/Main/src/ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS.java
+++ Etapa2/Main/src/ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS.java
@@ -3,32 +3,19 @@ package Etapa2.Main.src;
 import java.io.File;
 import java.util.Scanner;
 
-/*
-    1.	[Comparativo da LISTA DE CONCESSÃO DE CRÉDITO TRIBUTÁRIO (PIS/COFINS)] Com base somente nos produtos que foram comercializados em 2020, o programa deverá:
-    a.	Consultar a coluna de dados “LISTA DE CONCESSÃO DE CRÉDITO TRIBUTÁRIO (PIS/COFINS)” para determinar o percentual de produtos classificados como “Negativa”, “Neutra” ou “Positiva” para esta coluna.
-    b.	Mostrar os respectivos valores percentuais da seguinte maneira (dados fictícios):
-    [* repare que a quantidade de asteriscos é proporcional ao respectivo percentual, por exemplo, neste caso são 21 asteriscos para a classificação Negativa.]
-
-    CLASSIFICACAO
-    PERCENTUAL	GRAFICO
-    Negativa	21,33%	********************
-    Neutra	45,18%	*********************************************
-    Positiva	33,49%	*********************************
-*/
-
 public class ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS {
 
     // method that scans the csv file for the medicine acording to its name
     public void compareCreditPercentuals() {
 
-        final String FILE_PATH = "D:/Programming/GitHub/IT16/Etapa2/TA_PRECO_MEDICAMENTO.csv";
+        final String FILE_PATH = "Etapa2\\TA_PRECO_MEDICAMENTO.csv";
         File csvfile = new File(FILE_PATH);
 
         int negativeAmount = 0;
         int neutralAmount = 0;
         int positiveAmount = 0;
         String fileLine;
-        String regex = ",";
+        String regex = ";";
         String[] fileLineContent;
 
         try (Scanner reader = new Scanner(csvfile)) {
@@ -39,31 +26,52 @@ public class ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS {
                 fileLine = reader.nextLine();
                 fileLineContent = fileLine.split(regex);
 
-                if (fileLineContent[37].equals("Negativa")) {
-                    negativeAmount++;
-                } else if (fileLineContent[37].equals("Neutra")) {
-                    neutralAmount++;
-                } else if (fileLineContent[37].equals("Positiva")) {
-                    positiveAmount++;
-                } else {
-                    System.out.println("Erro: " + fileLine);
+                // since piscofins values are on column 37, I need the product to have at least
+                // this many columns
+                if (fileLineContent.length > 36) {
+                    if (fileLineContent[37].equals("Negativa")) {
+                        negativeAmount++;
+                    } else if (fileLineContent[37].equals("Neutra")) {
+                        neutralAmount++;
+                    } else if (fileLineContent[37].equals("Positiva")) {
+                        positiveAmount++;
+                    } else {
+                        System.out.println("Erro: " + fileLine);
+                    }
                 }
             }
 
-            createTable(negativeAmount, neutralAmount, positiveAmount);
+            createTable(calculatePercentages(negativeAmount, neutralAmount, positiveAmount));
 
         } catch (Exception e) {
             System.out.println("Houve um erro na leitura do arquivo. Tente novamente.");
         }
     }
 
-    // method that displays more information on the medicine
-    private void createTable(int negativeAmount, int neutralAmount, int positiveAmount) {
+    // method to calculate percentages
+    private double[] calculatePercentages(int negativeAmount, int neutralAmount, int positiveAmount) {
+        double[] percentages = new double[4];
+
         double total = (double) negativeAmount + neutralAmount + positiveAmount;
         double negativePercent = negativeAmount / total * 100;
         double neutralPercent = neutralAmount / total * 100;
         double positivePercent = positiveAmount / total * 100;
         total = negativePercent + neutralPercent + positivePercent;
+
+        percentages[0] = negativePercent;
+        percentages[1] = neutralPercent;
+        percentages[2] = positivePercent;
+        percentages[3] = total;
+
+        return percentages;
+    }
+
+    // method that displays more information on the medicine
+    private void createTable(double[] percentages) {
+        double negativePercent = percentages[0];
+        double neutralPercent = percentages[1];
+        double positivePercent = percentages[2];
+        double total = percentages[3];
 
         System.out.println("CLASSIFICAÇÃO\tPERCENTUAL\tGRAFICO");
         System.out.printf("Negativa\t%.2f%%\t\t%s %n", negativePercent, asteriscAmount(negativePercent));
@@ -72,6 +80,7 @@ public class ComparativoDaListaDeConcessaoDeCreditoTributarioPISCOFINS {
         System.out.printf("TOTAL\t\t%.2f%%", total);
     }
 
+    // method to return asterisc String
     private String asteriscAmount(double percent) {
         String asteriscs = "";
 
